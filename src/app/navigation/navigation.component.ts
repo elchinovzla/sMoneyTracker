349eee413a268ed385b76538e0e13d84f2d11ce8
elchinovzla <sonnyng.19@hotmail.ca>
2019-02-24 00:03:27 -0700

    Remove dynamic side menu - always on

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,14 +1,11 @@
 import {
-  ChangeDetectorRef,
   Component,
   OnInit,
   OnDestroy,
-  HostListener,
   ViewChild
 } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 import { Subscription } from 'rxjs';
-import { MediaMatcher } from '@angular/cdk/layout';
 import { MatSidenav } from '@angular/material';
 
 @Component({
@@ -17,25 +14,13 @@ import { MatSidenav } from '@angular/material';
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent implements OnInit, OnDestroy {
-  constructor(
-    private authService: AuthService,
-    changeDetectorRef: ChangeDetectorRef,
-    media: MediaMatcher
-  ) {
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
-    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-    this.mobileQuery.addListener(this._mobileQueryListener);
+  constructor(private authService: AuthService) {
     this.screenWidth = window.innerWidth;
   }
   @ViewChild('snav') sidenav: MatSidenav;
   private authListenerSubs: Subscription;
   userIsAuthentitcated = false;
   screenWidth: number;
-  mobileQuery: MediaQueryList;
-
-  fillerNav = Array.from({ length: 5 }, (_, i) => `Nav Item ${i + 1}`);
-
-  private _mobileQueryListener: () => void;
 
   ngOnInit() {
     this.userIsAuthentitcated = this.authService.getIsAuth();
@@ -47,17 +32,7 @@ export class NavigationComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-    this.mobileQuery.removeListener(this._mobileQueryListener);
     this.authListenerSubs.unsubscribe();
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onResize(event) {
-    if (event.target.innerWidth < 600) {
-      this.sidenav.close();
-    } else if (event.target.innerWidth > 840) {
-      this.sidenav.open();
-    }
   }
 
   onLogout() {
