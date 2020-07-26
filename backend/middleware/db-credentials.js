ac24be3de71e3b2aeb4552584709a60025b183a9
Sonny Ng <sonnyng.vzla@gmail.com>
2020-07-25 20:40:38 -0600

    update missing updates on backend

---
+++ backend/middleware/db-credentials.js
@@ -0,0 +1,3 @@
+const USER_NAME = 'DBusername';
+const PASSWORD = 'password';
+module.exports(USER_NAME, PASSWORD);
---
+++ backend/middleware/email-sender-credentials.js
@@ -0,0 +1,3 @@
+const USER_NAME = 'email@domain.com';
+const PASSWORD = 'emailPassword';
+module.exports = { USER_NAME, PASSWORD};
